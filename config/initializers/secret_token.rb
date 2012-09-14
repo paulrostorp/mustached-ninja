a17f0922210eebd5664f28c71223992ed5122ebe
paul rostorp <paul.rostorp@me.com>
2012-09-14 09:37:53 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aspsurvival::Application.config.secret_token = '166291e76dccf2a3ebc1bbfe428a86d3e1d92fe150d2206b4058094e602ee6db466ed0d222b94cd22b5c42fe9e102259c7d36e7713bb776aeb2e58baf3832ecb'
